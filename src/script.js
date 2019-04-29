37936397a1ad2ee923404263b292846a7480f344
Javaughn Burke <javaughnburke@Javaughns-MacBook-Air.local>
2019-04-29 03:44:10 -0400

    contact form

--- src/script.js
+++ src/script.js
@@ -1,3 +1,48 @@
+$(document).ready(function() {
+  "use strict"
+
+  $('.submit').click(function(event) {
+
+    var email = $('.email').val();
+    var name = $('.name').val();
+    var subject = $('.subject').val();
+    var message = $('.message').val();
+    var statusElm = $('.status');
+    statusElm.empty();
+
+    if(email.length > 5 && email.includes('@').includes('.')) {
+      statusElm.append('<div>Email is valid</div>');
+    }
+    else {
+      event.preventDefault();
+      statusElm.append('<div>Email is not valid</div>');
+    }
+
+
+    if(subject.length > 2) {
+      statusElm.append('<div>Subject is valid</div>');
+    }
+    else {
+      event.preventDefault();
+      statusElm.append('<div> Subject is not valid</div>');
+    }
+
+
+    if(message.length > 10) {
+      statusElm.append('<div>Message is valid</div>');
+    }
+    else{    
+      event.preventDefault();
+      statusElm.append('<div>Message not is valid</div>');
+    }
+
+
+  })
+});
+
+
+
+
 jQuery(document).ready(function() {
     "use strict"
 
@@ -42,6 +87,8 @@ jQuery(document).ready(function() {
         delay: 1000
 
     });
+
+    console.log("It works");
 
     $('a[href*="#"]')
     // Remove links that don't actually link to anything
